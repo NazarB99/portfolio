e3db8df106324d5219353b098ca6d30e213f7b20
NazarB99 <nazar99bayramsahatov@gmail.com>
2018-11-27 22:03:23 +0500

    upd

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ Route::get('/', function () {
     return view('welcome')->with('imgs',\App\Images::all())->with('main_img',\App\MainImage::find(1));
 });
 
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
